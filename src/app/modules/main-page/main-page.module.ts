ca64b27733209386f8483b7c969b04b8c81af6e6
Avdey <avdeev.1van@icloud.com>
2020-03-31 20:32:51 +0300

    Added main-page module and component

---
+++ src/app/modules/main-page/main-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageRoutingModule } from './main-page-routing.module';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+
+@NgModule({
+  declarations: [MainPageComponent],
+  imports: [
+    MainPageRoutingModule,
+    CommonModule,
+  ]
+})
+export class MainPageModule { }
