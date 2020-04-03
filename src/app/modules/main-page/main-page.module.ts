f8c5927b5e5d1fc0ef8f28ef7b4f93acdbe4b0ae
Avdey <avdeev.1van@icloud.com>
2020-04-03 17:49:57 +0300

    Added header

--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, HeaderComponent],
   imports: [MainPageRoutingModule, CommonModule],
 })
 export class MainPageModule {}
