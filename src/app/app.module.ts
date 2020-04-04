6ed0876966e7127f42f54ff3d77cf186e8e4e23d
Avdey <avdeev.1van@icloud.com>
2020-04-04 20:15:42 +0300

    added auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageModule } from './modules/main-page/main-page.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MainPageModule],
+  imports: [BrowserModule, AppRoutingModule, MainPageModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
