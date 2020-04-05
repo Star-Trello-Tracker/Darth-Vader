8b96713b86600ba2ceabbedc3e85468ed6fdd5d3
Avdey <avdeev.1van@icloud.com>
2020-04-05 13:44:44 +0300

    Added login page

--- src/app/modules/main-page/main-page-routing.module.ts
+++ src/app/modules/main-page/main-page-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes = [
   {
     path: '',
     component: MainPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [MainPageComponent, HeaderComponent],
-  imports: [MainPageRoutingModule, CommonModule],
+  declarations: [MainPageComponent, HeaderComponent, LoginComponent],
+  imports: [MainPageRoutingModule, CommonModule, FormsModule],
 })
 export class MainPageModule {}
