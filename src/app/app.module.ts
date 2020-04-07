47174466c01f26ff300b45f347f405a043575f91
Avdey <avdeev.1van@icloud.com>
2020-04-07 20:36:38 +0300

    Added dashboard module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageModule } from './modules/main-page/main-page.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MainPageModule],
+  imports: [BrowserModule, AppRoutingModule, MainPageModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
