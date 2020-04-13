c43e9803c1e452f7f9aa8456c4b3bd94417ca142
Ivan Avdeev <avdeev.1van@icloud.com>
2020-04-13 20:41:36 +0300

    Added filters in task list on default dashboard page

--- src/app/modules/dashboard/components/task-list/dashboard-tasks-list/dashboard-tasks-list.component.ts
+++ src/app/modules/dashboard/components/task-list/dashboard-tasks-list/dashboard-tasks-list.component.ts
@@ -8,8 +8,41 @@ import { data, ITask } from './data';
 })
 export class DashboardTasksListComponent implements OnInit {
   public list: ITask[] = data;
+  public data: ITask[] = data;
+
+  public all = true;
+  public author = false;
+  public person = false;
+  public observer = false;
+
+  private personData = 'Авдеев Иван';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public toggle(item: string) {
+    if (item !== 'all') {
+      this.all = false;
+    } else {
+      this.author = false;
+      this.person = false;
+      this.observer = false;
+    }
+    this[item] = !this[item];
+    this.list = this.filterData();
+  }
+
+  private filterData() {
+    if (this.all) {
+      return this.data;
+    }
+    return this.data.filter((el) => {
+      return (
+        (this.author && el.creator === this.personData) ||
+        (this.person && el.person === this.personData) ||
+        (this.observer && el.observer === this.personData)
+      );
+    });
+  }
 }
