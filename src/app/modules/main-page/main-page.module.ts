ac50d6a67cc818e52788d921bba3d9b56a37185e
Ivan Avdeev <avdeev.1van@icloud.com>
2020-05-09 19:27:51 +0300

    Made 1 header in app

--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
-  declarations: [
-    MainPageComponent,
-    HeaderComponent,
-    LoginComponent,
-    RegistrationComponent,
-  ],
+  declarations: [MainPageComponent, LoginComponent, RegistrationComponent],
   imports: [MainPageRoutingModule, CommonModule, FormsModule],
   exports: [MainPageComponent],
 })
--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -4,12 +4,20 @@ import { AuthService } from '../../../../auth-services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss', './authorized-header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public get isAuth() {
+    return this.authService.isAuth.getValue();
+  }
+
+  public get id() {
+    return this.authService.getId();
+  }
+
   constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
   public logout() {
     this.authService.logout();
